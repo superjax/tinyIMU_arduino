62794194a8cdc3c043de90400987b9031a64cc8d
James Jackson <superjax08@gmail.com>
2015-02-12 17:01:32 -0700

    first commit

---
+++ tinyIMU.ino
@@ -0,0 +1,89 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
+// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//      2013-05-08 - added multiple output formats
+//                 - added seamless Fastwire support
+//      2011-10-07 - initial release
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+int16_t ax, ay, az, gx, gy, gz;
+
+#include <ros.h>
+#include <ros/time.h>
+#include <axon_msgs/tinyVector.h>
+#include <axon_msgs/tinyImu.h>
+ros::NodeHandle  nh;
+
+axon_msgs::tinyImu imu_msg;
+ros::Publisher imu_pub("tinyImu", &imu_msg);
+
+uint32_t seq;
+
+void setup()
+{
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+  nh.initNode();
+  nh.advertise(imu_pub);
+  
+  accelgyro.initialize();
+  seq = 0;
+}
+
+void loop()
+{
+  seq++;
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  imu_msg.Header.stamp = nh.now();
+  imu_msg.Header.frame_id = 0;
+  imu_msg.Header.seq = seq;
+
+  imu_msg.accel.x = ax;
+  imu_msg.accel.y = ay;
+  imu_msg.accel.z = az;
+  imu_msg.gyro.x = gx;
+  imu_msg.gyro.y = gy;
+  imu_msg.gyro.z = gz;
+
+  imu_pub.publish( &imu_msg );
+  nh.spinOnce();
+}
